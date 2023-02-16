477d1eea21acc3d3472357120be1b7097df74acb
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-16 06:51:49 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ *main-prints string required
+ */
 int main(void)
 {
 	char Str[] = "Programming is like building a multilingual puzzle";
+
 	puts(Str);
 
-	return 0;
+	return (0);
 }
