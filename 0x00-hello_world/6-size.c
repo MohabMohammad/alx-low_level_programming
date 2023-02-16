19f2753250ffdcf96a185394264d75c2b3fc9457
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-16 08:12:07 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-	char c = 'a';
-	int i = 2;
-	long int li = 2;
-	long long int lli = 2;
-	float f = 2.0;
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
 
-	printf("Size of a char: %lu\n",(unsigned long)sizeof(&c));
-	printf("Size of an int: %lu\n",(unsigned long)sizeof(&i));
-	printf("Size of a long int: %lu\n",(unsigned long)sizeof(&li));
-	printf("Size of a long long int: %lu\n",(unsigned long)sizeof(&lli));
-	printf("Size of a float: %lu\n",(unsigned long)sizeof(&f));
+	printf("Size of a char: %lu\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu\n", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu\n", (unsigned long)sizeof(f));
 	return (0);
 }
