93c8eece9a81df78a64760ba1e2a06482175c425
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-16 07:16:41 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main-prints required string
+ *return:signifies no errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
