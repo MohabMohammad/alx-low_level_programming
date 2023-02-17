7847d0e09474320af30de592753410a5593b9ff8
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-17 08:14:56 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+/**
+ *main- prints digits from 0 to 9
+ *Return: 0 signifies no error
+ */
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a += 1;
+	}
+
+	printf("\n");
+
+	return (0);
+
+
+}
