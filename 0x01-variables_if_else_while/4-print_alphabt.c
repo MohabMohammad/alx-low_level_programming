9b7f357bdb3b8674e1b3b6ddd962f259c9463c05
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-17 07:52:52 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ *main- prints lowercase characters except q & e
+ *Return: 0 signifies no error
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
