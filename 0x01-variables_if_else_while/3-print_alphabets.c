f1d0aaaa426f169a9a9a61f2be354e917a80d9d9
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-17 07:41:46 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main-prints lowercase letters then uppercase letters
+ *Return: 0 signifies a new error
+ */
+int main(void)
+{
+	char c = 'a';
+	char c2 = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c = c + 1;
+	}
+
+	while (c2 < 'Z')
+	{
+		putchar(c2);
+		c2 =  c2 + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
