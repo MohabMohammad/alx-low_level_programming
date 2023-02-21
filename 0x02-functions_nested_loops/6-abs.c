6330c0bbc5aa0b92471edaf17eb77d9cbc49a015
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-21 06:35:40 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ *_abs- returns absolute value of number
+ *Return: absolute value
+ *@n: number ti be processed
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int abs = n * -1;
+
+		return (abs);
+	}
+}
