8e12d8aaf0665b5dc66a39f282db66c523faf9ec
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-21 05:50:35 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+/**
+ *_islower- determines whether character is lowercase
+ *Return: 1 if lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	return (0);
+}
