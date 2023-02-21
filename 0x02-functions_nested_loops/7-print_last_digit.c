9d6ff5dabd0366d0cc81385e50bdf7a268d8b513
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-21 06:47:53 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_last_digit- prints last digit of number
+ *Return: 0 signifies no error
+ *@n: number to get last digit from
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	_putchar(last);
+
+	return (0);
+}
