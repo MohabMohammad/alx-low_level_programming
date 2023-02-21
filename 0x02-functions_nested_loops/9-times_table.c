66885f9b24e472e43040802d887a8e929eacad34
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-21 10:51:04 -0800

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,27 @@
  */
 void times_table(void)
 {
-	int l = 0;
 	int i;
+	int j;
+	int product;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		int r = 0;
-		int j;
+		_putchar('0');
 
-		for(j = 0; j <= 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			r = r + l;
-			_putchar('0' + r);
 			_putchar(',');
 			_putchar(' ');
-			_putchar(' ');
-		}
 
-		l = l + 1;
+			product = i * j;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }
