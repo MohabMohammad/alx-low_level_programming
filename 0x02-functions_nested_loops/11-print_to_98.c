10fbf33cb2f389a1b9371dafb4b30e39a2ce9870
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-21 09:28:32 -0800

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *print_to_98- prints from given number to 98
+ *Return: void
+ *@n: number to start counting from
+ */
+void print_to_98(int n)
+{
+	int i;
+	int j;
+
+	printf(n);
+	printf(',');
+	printf(' ');
+
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+			printf(i);
+			printf(", ");
+		printf("98");
+	}
+	else if (n < 98)
+	{
+		for (j = n; j < 98; j++)
+		{
+			printf(j);
+			printf(", ");
+		}
+		printf("98");
+	}
+	else
+		printf("98");
+}
