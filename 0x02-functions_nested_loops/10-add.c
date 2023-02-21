f7945452c156ea7a903b6a07b972d2582bdcdc85
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-21 08:12:10 -0800

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ *add- adds two numbers
+ *Return: sum of two numbers
+ *@a: first number to be added
+ @b: second number to be added
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	
+	return (sum);
+}
