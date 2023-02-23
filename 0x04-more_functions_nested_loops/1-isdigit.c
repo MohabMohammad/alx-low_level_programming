88962189de8285e411284a7b92880da8761da4ec
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-23 01:16:55 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+/**
+ *_isdigit- checks whether parameter is a digit 0-9
+ *Return- 1 if digit, 0 otherwise
+ *@c: parameter to be checked if digit
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
