56e72eec75699cb0ace14651e87ab7e1ca98d366
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-23 14:08:08 -0800

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,16 @@ void print_diagonal(int n)
         int i;
         int j;
 
-        for (i = 0; i <= n; ++i)
-        {
-                for (j = 0; j <= n - 1; ++j)
-                {
-                        _putchar(' ');
-                }
-                _putchar('\\');
-                _putchar('\n');
-        }
-        _putchar('\n');
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < n; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
