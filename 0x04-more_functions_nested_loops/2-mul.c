80423b4ee41a290992658cb3e666188eee45ccfb
Mohab Mohammad <mohab.fcai@gmail.com>
2023-02-23 01:22:18 -0800

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ *mul- multiplies 2 numbers
+ *Return: product of multiplication
+ *@a: 1st number to be multiplied
+ *@b: 2nd number to be multiplied
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
